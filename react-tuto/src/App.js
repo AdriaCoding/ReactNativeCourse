fe5ac2512cfc159214dc44f77b600810149cb005
AdriaCoding <adrialisa99@gmail.com>
2025-02-03 14:51:44 +0100

    React does not re-render on every event. We have to useState

--- react-tuto/src/App.js
+++ react-tuto/src/App.js
@@ -2,16 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import GoalList from './components/GoalList/GoalList';
 import NewGoal from './components/NewGoal/NewGoal';
+import React, { useState } from 'react';
 function App() {
-  const courseGoals = [
+  // useState always returns an array with 2 elements
+  const [courseGoals, setCourseGoals] =  useState([
     {id: 'cg1', text: 'Finish the Course'},
     {id: 'cg2', text: 'Learn all about the Course Main topic'},
     {id: 'cg3', text: 'Help other students in the course Q&A'}
-  ];
+  ]);
 
   const addNewGoalHandler = (newGoal) => {
-    courseGoals.push(newGoal);
-    console.log(courseGoals);
+    //use concat instead of push because react wants 
+    // us to return a new array to re-render the component.
+    setCourseGoals(courseGoals.concat(newGoal));
   }
 
   return (
