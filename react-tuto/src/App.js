42ead208f42ebcb52fd13a0c7cd7c8578127972f
AdriaCoding <adrialisa99@gmail.com>
2025-02-03 13:22:51 +0100

    Splitting a component into a separete file

--- react-tuto/src/App.js
+++ react-tuto/src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import GoalList from './components/GoalList';
 function App() {
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <ul className="goal-list">
-        <li>Finish The course</li>
-        <li>Learn all about the Course Main topic</li>
-        <li>Help other students in the course Q&A</li>
-      </ul>
+      <GoalList/>
     </div>
   );
 }
---
+++ react-tuto/src/components/GoalList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './GoalList.css';
+const GoalList = () => {
+    return (
+        <ul className="goal-list">
+            <li>Finish The course</li>
+            <li>Learn all about the Course Main topic</li>
+            <li>Help other students in the course Q&A</li>
+        </ul>
+    );
+};
+
+export default GoalList;
