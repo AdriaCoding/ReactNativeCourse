f2fb24360635f8e037cc242bcd8d805a09f2d207
AdriaCoding <adrialisa99@gmail.com>
2025-02-03 13:30:01 +0100

    goals is a JSX prop that holds a reference to the javascript object courseGoals

--- react-tuto/src/App.js
+++ react-tuto/src/App.js
@@ -2,10 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import GoalList from './components/GoalList';
 function App() {
+  const courseGoals = [
+    {id: 'cg1', text: 'Finish the Course'},
+    {id: 'cg2', text: 'Learn all about the Course Main topic'},
+    {id: 'cg3', text: 'Help other students in the course Q&A'}
+  ];
+
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <GoalList/>
+      <GoalList goals={courseGoals}/>
     </div>
   );
 }
