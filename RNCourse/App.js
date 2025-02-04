fba60f8f2a5d0fb32b9f28643db94c980f3a2164
AdriaCoding <adrialisa99@gmail.com>
2025-02-04 13:45:36 +0100

    We needed to set flex:1 to the root View container in order for it to occupy al the available space in the screen.

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -5,9 +5,9 @@ export default function App() {
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput placeholder='Your course goal!' style={styles.textInput}/>
-        <Button title="add goal" />
+        <Button title="Add goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>Your course goals...</Text>
       </View>
     </View>
@@ -16,13 +16,19 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1,
     padding: 50,
+    paddingHorizontal: 16,
     backgroundColor: '#fff'
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
   },
   textInput :{
     borderWidth: 1,
@@ -30,5 +36,8 @@ const styles = StyleSheet.create({
     width: '80%',
     marginRight: 10,
     padding: 8
+  },
+  goalsContainer: {
+    flex: 4,
   }
 });
