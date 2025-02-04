46f1801c16d4f6d7538d41e4e22d0a66cb9c6fa9
AdriaCoding <adrialisa99@gmail.com>
2025-02-04 13:28:18 +0100

    Flexbox introduction: Interestingly, every View RN component uses flex alignment

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,28 +1,34 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.textbox}>Marti</Text>
-      <Text style={styles.textbox}>Hello World!</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Your course goal!' style={styles.textInput}/>
+        <Button title="add goal" />
+      </View>
       <View>
-        <Button title='Pínchame '/>
+        <Text>Your course goals...</Text>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
+    backgroundColor: '#fff'
   },
-  textbox: {
-    margin: 20,
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  textInput :{
     borderWidth: 1,
-    borderColor: 'red',
-    padding: 23
+    borderColor: '#cccccc',
+    width: '80%',
+    marginRight: 10,
+    padding: 8
   }
 });
