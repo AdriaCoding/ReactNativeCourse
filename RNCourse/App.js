7f1dedc67895aac27f35af7cd66727d91365de12
AdriaCoding <adrialisa99@gmail.com>
2025-02-04 14:29:03 +0100

    Adding a basic ScrollView element to scroll the list

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,5 +1,12 @@
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { 
+  Button, 
+  StyleSheet, 
+  Text, 
+  TextInput, 
+  View, 
+  ScrollView
+} from 'react-native';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -32,13 +39,15 @@ export default function App() {
         />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal) =>
-          <View style={styles.goalItem} key={goal}>
-            <Text style={styles.goalText}>
-              {goal}
-            </Text>
-          </View>
-        )}
+        <ScrollView>
+          {courseGoals.map((goal) =>
+            <View style={styles.goalItem} key={goal}>
+              <Text style={styles.goalText}>
+                {goal}
+              </Text>
+            </View>
+          )}
+        </ScrollView>
       </View>
     </View>
   );
