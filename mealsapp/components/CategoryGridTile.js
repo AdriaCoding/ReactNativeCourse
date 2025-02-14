b38434579a8843e2255438092631e818d52f0053
AdriaCoding <adrialisa99@gmail.com>
2025-02-14 10:04:14 +0100

    Add styles to navigation component. Button ripple effect only works on android.

--- mealsapp/components/CategoryGridTile.js
+++ mealsapp/components/CategoryGridTile.js
@@ -1,13 +1,43 @@
-import { Pressable, View, Text} from "react-native"
+import { Pressable, View, Text, StyleSheet, Button } from "react-native";
 
-function CategoryGridTile({title, color}) {
-  return <View>
-    <Pressable>
-      <View>
-        <Text>{title}</Text>
-      </View>
-    </Pressable>
-  </View>
+function CategoryGridTile({ title, color }) {
+  return (
+    <View style={styles.gridItem}>
+      <Pressable style={styles.button} android_ripple={{ color: "lightgrey" }}>
+        <View style={styles.innerContainer}>
+          <Text>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
 }
 
 export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 1, height: 2 },
+    shadowRadius: 8,
+    backgroundColor: "white"
+  },
+  button: {
+    flex: 1
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22
+  }
+});
